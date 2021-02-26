7f502667812116cfa470ee63446bd7229e1bbe97
Keun-Woo <rjsdnsla2244@naver.com>
2021-02-26 09:50:22 +0900

    수박수 완료

--- src/CORDINGTEST/subaksu.java
+++ src/CORDINGTEST/subaksu.java
@@ -5,9 +5,25 @@ public class subaksu {
 	public static void main(String[] args) {
 		String su ="수";
 		String bak = "박";
-		int a = (int)Math.random()*100;
-		while()
-
+		int a = (int)(Math.random()*100);
+		int b = a;
+		int count =1;
+		String answer ="";
+		while(count <=a){
+			if(count%2==1){
+			answer +=su;
+			}
+			else {
+				answer +=bak;
+			
+			}
+			
+			count +=1;
+			b -=1;
+		}
+		System.out.println("a는"+a+"이고 답은 : "+answer );
+		
 	}
-
+	
 }
+
